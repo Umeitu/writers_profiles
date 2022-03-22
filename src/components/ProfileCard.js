64a152ce672143b2df59b87bc9b39affe9ac8e75
Umeitu <umeitupelpuo@gmail.com>
2022-03-22 11:19:46 +0000

    added data

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function ProfileCard({writer}) {
+
+function profileCard({writer}) {
     return (
-        <div className='card'>
-            <img src= {`images/${writer.avatar}.png`}
-            height="300px" width="300px" alt={writer.img}/>
+     <div className='card'>
+        <img src= {`Images/${writer.avatar}.png`}
+        height="300px width=300px" alt={writer.img}/>
 
-            <div className='textGroup'>
-                <h3>{writer.name}</h3>
-                <p>{writer.email}</p>
-                <p>{writer.phone}</p>
 
-                <button className='actionBtn'>Read Bio</button>
-            </div>
-        </div>
-    )
-    
+<div className='textGroup'>
+    <h3>{writer.name}</h3>
+    <p>{writer.email}</p>
+    <p>{writer.phone}</p>
+
+    <button className='actionBtn'>read Bio</button> 
+
+
+    </div> 
+    </div>
+
+)
+
 }
-export default ProfileCard;
+export default profileCard;
